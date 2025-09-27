8a0ff411ba7a1817dadd568a3432ed618208da72
Luis Filipe <135190249+LuisFilipeOliveiraSantos@users.noreply.github.com>
2025-09-27 20:57:48 -0300

    Update auth.py

--- backend/app/auth/auth.py
+++ backend/app/auth/auth.py
@@ -68,7 +68,7 @@ password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def get_hashed_password(password: str) -> str:
-    return password_context.hash(password)
+    return password_context.hash(password[:72])
 
 
 def verify_password(password: str, hashed_pass: str) -> bool:
