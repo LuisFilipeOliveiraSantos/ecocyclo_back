fdb960c08ab2d8211b8fe56daf08bed01a3ae7ce
Luis Filipe <135190249+LuisFilipeOliveiraSantos@users.noreply.github.com>
2025-10-10 20:28:12 -0300

    Update auth_company.py

--- backend/app/auth/auth_company.py
+++ backend/app/auth/auth_company.py
@@ -46,10 +46,7 @@ oauth2_scheme_with_cookies = OAuth2PasswordBearerWithCookie(tokenUrl=f"{settings
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_hashed_password(password: str) -> str:
-    # Proteção extra - truncar se maior que 72 bytes
-    if len(password.encode('utf-8')) > 72:
-        password = password.encode('utf-8')[:72].decode('utf-8', 'ignore')
-    return password_context.hash(password)
+    return password_context.hash(password[:72])
 
 def verify_password(password: str, hashed_pass: str) -> bool:
     return password_context.verify(password, hashed_pass)
