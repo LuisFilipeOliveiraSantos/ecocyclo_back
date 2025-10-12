d58d89825cb1911cbeb14c3db928e4b046eb4326
Luis Filipe <135190249+LuisFilipeOliveiraSantos@users.noreply.github.com>
2025-10-12 18:44:29 -0300

    Update api.py

--- backend/app/routers/api.py
+++ backend/app/routers/api.py
@@ -3,14 +3,13 @@ from fastapi import APIRouter
 from . import login, users
 from . import company_auth, company
 from .  import password_reset
-from . import items_recognition
+
 api_router = APIRouter()
 api_router.include_router(login.router, prefix="/login", tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(company_auth.router, prefix="/company/login", tags=["company auth"])
 api_router.include_router(company.router, prefix="/company", tags=["company"])
 api_router.include_router(password_reset.router, prefix="/company/resetPassword", tags=["company password reset"])
-api_router.include_router(items_recognition.router, prefix="/itemRecognition", tags = ["Computer vision model"])
 
 @api_router.get("/")
 async def root():
